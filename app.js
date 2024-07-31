81c2c7d990938737432cbfb42a06802c3cb44198
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-07-31 19:57:08 +0530

    chore: Refactor app.js to remove unnecessary code

--- app.js
+++ app.js
@@ -3,12 +3,9 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) =>{
-  res.send('Hello World');
+  res.send();
 });
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
-
-
