b4d37e63d68f312019986897b88ca06856f64475
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-08-01 23:22:26 +0530

    chore: Update app.js to include socket.io functionality

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
