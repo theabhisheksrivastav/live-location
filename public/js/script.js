78a33876af47a9f92d52afdc9df8c472c48e38c2
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-08-01 23:35:15 +0530

    chore: Add geolocation functionality for real-time location updates

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,14 @@
 const socket = io();
+
+if(navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit('sendLocation', { latitude, longitude });
+    }, (error) => {
+        console.log(error);
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000
+    });
+}
