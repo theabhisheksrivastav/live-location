b45c052a4d39fdd68ef25a743506da2ff3ebd4a4
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-08-01 23:31:29 +0530

    chore: Update app.js to render index.ejs instead of sending 'Hello World!'

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index');
 });
 
 io.on('connection', (socket) => {
