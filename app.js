28ff1ecc5b404d3a77bd91f490670888e3fefa34
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-08-02 12:18:47 +0530

    chore: Update app.js to include client-side geolocation functionality for real-time location updates

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = new Server(server);
 
-
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -21,6 +23,10 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
+  });
+
+  socket.on('sendLocation', (location) => {
+    console.log(`Location received: ${location.latitude}, ${location.longitude}`);
   });
 });
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
+
+console.log('Client side javascript file is loaded!');
 const socket = io();
+
 
 if(navigator.geolocation) {
     navigator.geolocation.watchPosition((position) => {
